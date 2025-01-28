296b6b7ecdbf71384323e9cffe9848acb16c1dcf
M1roel <ppfautsch@yahoo.de>
2025-01-28 07:28:36 +0100

    refactor: replace mat-drawer with standalone sidenav and dashboard components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidenavComponent } from "./components/sidenav/sidenav.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, MatButtonModule, MatSidenavModule],
+  imports: [CommonModule, RouterModule, SidenavComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
