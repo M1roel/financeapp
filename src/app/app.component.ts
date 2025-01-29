35ff8c7b62ea7c778e800ba3a64d438328ba6bb2
M1roel <ppfautsch@yahoo.de>
2025-01-29 19:37:26 +0100

    feat: add language toggle feature and translation support for sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from "@ngx-translate/core";
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +15,26 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     RouterModule,
     RouterOutlet,
     MatButtonModule,
-    MatSidenavModule
+    MatSidenavModule,
+    TranslateModule,
+    MatButtonToggleModule
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'financeapp';
+
+  currentLanguage = signal('en');
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use(this.currentLanguage());
+  }
+
+  useLanguage(language: string): void {
+    this.currentLanguage.set(language);
+    this.translate.use(language);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    importProvidersFrom(BrowserAnimationsModule),
+    ...appConfig.providers, // Lade die vorhandenen Provider
+    importProvidersFrom(HttpClientModule), // HttpClientModule direkt importieren
+    provideAnimations(),
   ],
 }).catch((err) => console.error(err));
