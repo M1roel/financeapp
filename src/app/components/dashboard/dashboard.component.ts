9ea42e443235ff3ed25a2ec8e9b28996f9137cad
M1roel <ppfautsch@yahoo.de>
2025-01-29 19:53:56 +0100

    feat: add translation support for dashboard titles and balance items

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
