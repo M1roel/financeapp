5ffbadb40b11d3d8f98479353fdee977acacedb8
M1roel <ppfautsch@yahoo.de>
2025-01-30 08:19:46 +0100

    feat: add Firebase configuration and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers, // Lade die vorhandenen Provider
     importProvidersFrom(HttpClientModule), // HttpClientModule direkt importieren
-    provideAnimations(),
+    provideAnimations()importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"financeapp-ae3f4","appId":"1:779509874066:web:3e45a45ebf9fcef4d1954b","storageBucket":"financeapp-ae3f4.firebasestorage.app","apiKey":"AIzaSyCqPhWMqujwbsPY9zLYmmlK4dOd_h90A0U","authDomain":"financeapp-ae3f4.firebaseapp.com","messagingSenderId":"779509874066"})))importProvidersFrom(provideAuth(() => getAuth()))importProvidersFrom(provideFirestore(() => getFirestore())),
   ],
 }).catch((err) => console.error(err));
