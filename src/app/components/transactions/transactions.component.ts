34e550f9a34bb17fdc4849d1a6122ca41c5ec9a7
M1roel <ppfautsch@yahoo.de>
2025-02-09 14:51:39 +0100

    feat: enhance transactions component with search and sorting functionality

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { TranslateService } from "@ngx-translate/core";
+import { TranslateService } from '@ngx-translate/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, MatSelectModule, MatFormFieldModule],
   templateUrl: './transactions.component.html',
-  styleUrl: './transactions.component.scss'
+  styleUrl: './transactions.component.scss',
 })
-export class TransactionsComponent {
-
-}
+export class TransactionsComponent {}
