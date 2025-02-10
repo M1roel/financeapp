3c43ce5f7b9d22df77ba815f351d27caf773535c
M1roel <ppfautsch@yahoo.de>
2025-02-10 12:45:08 +0100

    feat: enhance add pot dialog with translations and input fields

--- src/app/dialogs/dialog-add-pot/dialog-add-pot.component.ts
+++ src/app/dialogs/dialog-add-pot/dialog-add-pot.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dialog-add-pot',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './dialog-add-pot.component.html',
   styleUrl: './dialog-add-pot.component.scss'
 })
