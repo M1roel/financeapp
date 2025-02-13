aa2015942fbcd22ddb1982bdbe7924fe915ab4a9
M1roel <ppfautsch@yahoo.de>
2025-02-13 15:04:50 +0000

    feat: implement pot data retrieval in PotsComponent and initialize pot model

--- src/app/components/pots/pots.component.ts
+++ src/app/components/pots/pots.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { TranslateService } from "@ngx-translate/core";
 import { TranslateModule } from '@ngx-translate/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddPotComponent } from '../../dialogs/dialog-add-pot/dialog-add-pot.component';
+import { Pot } from '../../models/pot.class';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 
 
 @Component({
@@ -12,9 +14,26 @@ import { DialogAddPotComponent } from '../../dialogs/dialog-add-pot/dialog-add-p
   templateUrl: './pots.component.html',
   styleUrl: './pots.component.scss'
 })
-export class PotsComponent {
+export class PotsComponent implements OnInit {
 
-  constructor(private translate: TranslateService, public dialog: MatDialog) {}
+  pot = new Pot();
+
+  constructor(private translate: TranslateService, public dialog: MatDialog, @Inject(Firestore) private firestore: Firestore) {}
+  
+
+  ngOnInit(): void {
+    const potCollection = collection(this.firestore, 'pots');
+
+    collectionData(potCollection, { idField: 'id' }).subscribe(
+      (changes: any) => {
+        console.log(changes);
+
+        if (changes.length > 0) {
+          Object.assign(this.pot, changes[0]);
+        }
+      }
+    );
+  }
 
   openDialog() {
     this.dialog.open(DialogAddPotComponent);
