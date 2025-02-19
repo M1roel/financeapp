c29ffd096489f312c62aa3721ff3b7c37e153003
M1roel <ppfautsch@yahoo.de>
2025-02-19 15:13:40 +0100

    feat: enhance add money dialog with amount input binding and confirmation button

--- src/app/dialogs/pot/dialog-add-money/dialog-add-money.component.ts
+++ src/app/dialogs/pot/dialog-add-money/dialog-add-money.component.ts
@@ -1,24 +1,41 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  MatDialogModule,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { TranslateModule } from '@ngx-translate/core';
 import { Pot } from '../../../models/pot.class';
-import { Firestore, doc, updateDoc, collection, collectionData } from '@angular/fire/firestore';
+import {
+  Firestore,
+  doc,
+  updateDoc,
+  collection,
+  collectionData,
+} from '@angular/fire/firestore';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-add-money',
   standalone: true,
-  imports: [TranslateModule, MatDialogModule, MatProgressBarModule, CommonModule],
+  imports: [
+    TranslateModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './dialog-add-money.component.html',
-  styleUrl: './dialog-add-money.component.scss'
+  styleUrl: './dialog-add-money.component.scss',
 })
 export class DialogAddMoneyComponent {
-
   pot = new Pot();
   allPots: any[] = [];
+  amountToAdd: number = 0;
 
-  constructor (
+  constructor(
     @Inject(Firestore) private firestore: Firestore,
     @Inject(MAT_DIALOG_DATA) public data: Pot,
     private dialogRef: MatDialogRef<DialogAddMoneyComponent>
@@ -38,8 +55,28 @@ export class DialogAddMoneyComponent {
       }
     );
   }
-  
+
   closeDialog() {
     this.dialogRef.close();
+  }
+
+  async updateTotalSaved(pot: Pot) {
+    if (this.data?.id) {
+      if (this.amountToAdd <= 0) {
+        console.error('Bitte einen gültigen Betrag eingeben.');
+        return;
+      }
+      const newTotalSaved = this.pot.total_saved + this.amountToAdd;
+      const potDocRef = doc(this.firestore, `pots/${this.data.id}`);
+      try {
+        await updateDoc(potDocRef, { total_saved: newTotalSaved });
+        this.pot.total_saved = newTotalSaved;
+        this.closeDialog();
+      } catch (error) {
+        console.error('Fehler beim Aktualisieren von total_saved:', error);
+      }
+    } else {
+      console.error('Kein pot-Datenobjekt verfügbar.');
+    }
   }
 }
